b46eb692e2eb770d510c7bea62d5f34ff473e6e9
David <isdnhk@naver.com>
2024-06-26 16:40:54 +0900

    [mission] feat : 내가 작성한 review 목록 조회

--- src/mission/umc/src/main/java/com/example/umc/repository/ReviewRepository.java
+++ src/mission/umc/src/main/java/com/example/umc/repository/ReviewRepository.java
@@ -1,8 +1,11 @@
 package com.example.umc.repository;
 
 import com.example.umc.domain.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
-
+    //특정 사용자가 작성한 리뷰를 페이징 처리하여 가져오는 메서드 추가
+    Page<Review> findByMemberId(Long memberId, Pageable pageable);
 }
